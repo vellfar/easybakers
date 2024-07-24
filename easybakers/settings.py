66e6fbfeeaa0467e4050d76688e0f130a7a31bd2
ibrahim.shuaib <sempijjahanni001@gmail.com>
2024-07-25 00:38:19 +0300

    build config

--- easybakers/settings.py
+++ easybakers/settings.py
@@ -119,6 +119,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())

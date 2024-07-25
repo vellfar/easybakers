86d60bd1731506323726a7432d8570ca49fb3349
ibrahim.shuaib <sempijjahanni001@gmail.com>
2024-07-25 08:34:00 +0300

    files build

--- easybakers/settings.py
+++ easybakers/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
